12c459bdb0c4320c2fd8f2ca1bf41969ed98b049
sasadangelo <sasadangelo@gmail.com>
2019-09-24 10:03:58 +0200

    Rename main script

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from setuptools import setup
 
 setup(
-    name='patroni_exporter',
+    name='patroni-exporter',
     version='0.0.6',
     description='Export Patroni metrics in Prometheus format',
     author='Jan Tomsa',
     author_email='ops@showmax.com',
-    scripts=['patroni_exporter.py'],
+    scripts=['patroni-exporter.py'],
 )
